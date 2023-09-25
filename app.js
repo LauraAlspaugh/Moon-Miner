6718f04a8f8ce3cd21b7b99a0066b448432e1439
Laura Alspaugh <lauraalspaugh2002@icloud.com>
2023-09-25 16:47:51 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+let home = 0
+let away = 0
+let bottomScore = 0
+let newScore = 0
+
+
+
+
+
+
+
+
+function homePlusOne() {
+    home++
+    console.log('score increase', home);
+    let scoreElm = document.getElementById('homeScore')
+    // @ts-ignore
+    scoreElm.innerText = home
+}
+
+function awayPlusOne() {
+    away++
+    console.log('score increase', away);
+    let scoreElm = document.getElementById('awayScore')
+    scoreElm.innerText = away
+}
+function homePlusThree() {
+    home += 3
+    console.log('score increase by three', home);
+    let scoreElm = document.getElementById('homeScore')
+    scoreElm.innerText = home
+}
+function awayPlusThree() {
+    away += 3
+    console.log('score increase by three', away);
+    let scoreElm = document.getElementById('awayScore')
+    scoreElm.innerText = away
+}
+function resetButton() {
+
+    if (home > 0) {
+        console.log('new score', newScore);
+    }
+}
